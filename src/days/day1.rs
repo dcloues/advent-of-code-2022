4f32de4e05a2bd383c5b9b141b96123d95fbe15c
Dustin Shields-Cloues <dustin@shields.clou.es>
2022-12-01 23:08:24 -0500

    Golf Day 1

--- src/days/day1.rs
+++ src/days/day1.rs
@@ -1,23 +1,15 @@
-use std::error::Error;
+use std::{error::Error, iter::from_fn};
 
 fn sum_batches(input: &str) -> Result<Vec<i64>, Box<dyn Error>> {
-    let mut batch_totals: Vec<i64> = input
-        .lines()
-        .map(str::trim)
-        .try_fold(
-            vec![vec![]],
-            |mut acc: Vec<Vec<i64>>, l: &str| -> Result<_, Box<dyn Error>> {
-                if l.is_empty() {
-                    acc.push(vec![]);
-                } else {
-                    acc.last_mut().unwrap().push(l.parse()?);
-                }
-                Ok(acc)
-            },
-        )?
-        .iter()
-        .map(|b| b.iter().sum())
-        .collect();
+    let mut lines = input.lines().map(str::trim);
+    let mut batch_totals: Vec<i64> = from_fn(|| {
+        let mut batch = lines.by_ref().take_while(|l| !l.is_empty()).peekable();
+        batch
+            .peek()
+            .is_some()
+            .then(|| batch.map(|l| l.parse::<i64>()).sum())
+    })
+    .collect::<Result<_, _>>()?;
 
     batch_totals.sort();
     Ok(batch_totals)
