74dc3b60fea0c11acc3c6e63f720a2506e1dd0b4
Dustin Shields-Cloues <dustin@shields.clou.es>
2022-12-01 21:17:44 -0500

    Day 1, Parts 1 and 2

--- src/days/day1.rs
+++ src/days/day1.rs
@@ -1,6 +1,63 @@
-use std::{io::Read, error::Error};
+use std::error::Error;
 
-pub fn run(_input: impl Read) -> Result<String, Box<dyn Error>> {
+fn sum_batches(input: &str) -> Result<Vec<i64>, Box<dyn Error>> {
+    let mut batch_totals: Vec<i64> = input
+        .lines()
+        .map(str::trim)
+        .try_fold(
+            vec![vec![]],
+            |mut acc: Vec<Vec<i64>>, l: &str| -> Result<_, Box<dyn Error>> {
+                if l.is_empty() {
+                    acc.push(vec![]);
+                } else {
+                    acc.last_mut().unwrap().push(l.parse()?);
+                }
+                Ok(acc)
+            },
+        )?
+        .iter()
+        .map(|b| b.iter().sum())
+        .collect();
 
-    Ok("Hello, Day 1".to_string())
+    batch_totals.sort();
+    Ok(batch_totals)
+}
+
+pub fn part1(input: &str) -> Result<String, Box<dyn Error>> {
+    sum_batches(input).map(|bs| format!("{}", bs.last().unwrap()))
+}
+
+pub fn part2(input: &str) -> Result<String, Box<dyn Error>> {
+    sum_batches(input).map(|bs| format!("{}", bs.iter().rev().take(3).sum::<i64>()))
+}
+
+#[cfg(test)]
+mod test {
+    use super::part1;
+    use super::part2;
+
+    const TEST_INPUT: &str = "1000
+    2000
+    3000
+    
+    4000
+   
+    5000
+    6000
+   
+    7000
+    8000
+    9000
+   
+    10000";
+
+    #[test]
+    fn test_part1() {
+        assert_eq!(part1(TEST_INPUT).unwrap(), "24000")
+    }
+
+    #[test]
+    fn test_part2() {
+        assert_eq!(part2(TEST_INPUT).unwrap(), "45000")
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,36 @@
 mod days;
 
-use std::env::{self, current_dir};
-use std::error::Error;
-use std::fs::File;
-use std::path::PathBuf;
 use days::day1;
+use std::env::{args, current_dir};
+use std::error::Error;
+use std::path::PathBuf;
 
-fn default_input(day: i32, relative: &PathBuf) -> std::io::Result<File> {
+fn default_input(day: i32, relative: &PathBuf) -> std::io::Result<String> {
     let path = relative.join("input").join(format!("day{}.txt", day));
-
-    return File::open(path)
+    std::fs::read_to_string(path)
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let args: Vec<String> = env::args().collect();
-    match args.len() {
-        2 => {
-            let day: i32 = args[1].parse()?;
-            let input: File = default_input(day, &current_dir()?)?;
+    let args: Vec<String> = args().collect();
 
-            let result = crate::day1::run(input)?;
-            println!("Day {}: {}", day, result);
+    match args.len() {
+        3 => {
+            let day: i32 = args[1].parse()?;
+            let part: i32 = args[2].parse()?;
+            let input: String = default_input(day, &current_dir()?)?;
+
+            let result = match part {
+                1 => crate::day1::part1(&input),
+                2 => crate::day1::part2(&input),
+                _ => Err("invalid part (must be 1 or 2".into()),
+            }?;
+            println!("Day {} Part {}: {}", day, part, result);
 
             Ok(())
-        },
+        }
         _ => {
             println!("Usage: adventofcode2022 day");
             Err("invalid arguments".into())
         }
     }
-
 }
