9b3198d9e2fd1f7d262c7bd496d3eaeb52e9ef31
Dustin Shields-Cloues <dustin@shields.clou.es>
2022-12-01 21:55:44 -0500

    Over-engineer day/part input

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ use std::env::{args, current_dir};
 use std::error::Error;
 use std::path::PathBuf;
 
-fn default_input(day: i32, relative: &PathBuf) -> std::io::Result<String> {
+fn default_input(day: usize, relative: &PathBuf) -> std::io::Result<String> {
     let path = relative.join("input").join(format!("day{}.txt", day));
     std::fs::read_to_string(path)
 }
@@ -13,19 +13,23 @@ fn default_input(day: i32, relative: &PathBuf) -> std::io::Result<String> {
 fn main() -> Result<(), Box<dyn Error>> {
     let args: Vec<String> = args().collect();
 
+    type Part = dyn Fn(&str) -> Result<String, Box<dyn Error>>;
+    let days: Vec<Vec<Box<Part>>> = vec![vec![Box::new(day1::part1), Box::new(day1::part2)]];
+
     match args.len() {
         3 => {
-            let day: i32 = args[1].parse()?;
-            let part: i32 = args[2].parse()?;
+            let day: usize = args[1].parse()?;
+            let part: usize = args[2].parse()?;
             let input: String = default_input(day, &current_dir()?)?;
 
-            let result = match part {
-                1 => crate::day1::part1(&input),
-                2 => crate::day1::part2(&input),
-                _ => Err("invalid part (must be 1 or 2".into()),
-            }?;
-            println!("Day {} Part {}: {}", day, part, result);
+            let f = days
+                .get(day - 1)
+                .ok_or(format!("invalid day {}", day))?
+                .get(part - 1)
+                .ok_or(format!("invalid part {} for day {}", part, day))?;
 
+            let result = f(&input)?;
+            println!("Day {} Part {}: {}", day, part, result);
             Ok(())
         }
         _ => {
