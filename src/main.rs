4aaf8f5a88b5c7e0dbc65173e90ee0d2be5c002f
Dustin Shields-Cloues <dustin@shields.clou.es>
2022-12-07 06:58:06 -0500

    Plumb day 7

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,7 @@ use days::day3;
 use days::day4;
 use days::day5;
 use days::day6;
+use days::day7;
 use std::env::{args, current_dir};
 use std::error::Error;
 use std::path::PathBuf;
@@ -26,6 +27,7 @@ fn main() -> Result<(), Box<dyn Error>> {
         vec![Box::new(day4::part1), Box::new(day4::part2)],
         vec![Box::new(day5::part1), Box::new(day5::part2)],
         vec![Box::new(day6::part1), Box::new(day6::part2)],
+        vec![Box::new(day7::part1), Box::new(day7::part2)],
     ];
 
     match args.len() {
