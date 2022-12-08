ac2337560bfcea9c4bb39499f4bd3b20ac06c2f8
Dustin Shields-Cloues <dustin@shields.clou.es>
2022-12-08 09:39:06 -0500

    Day 8, Part 1

--- src/days/day8.rs
+++ src/days/day8.rs
@@ -1,7 +1,65 @@
 use std::error::Error;
 
 pub fn part1(input: &str) -> Result<String, Box<dyn Error>> {
-    todo!("unimplemented")
+    let map: Vec<Vec<i8>> = input.lines()
+        .map(|l| l.chars().map(|c| c.to_digit(10).map(|d| d as i8)).collect())
+        .collect::<Option<Vec<Vec<_>>>>()
+        .ok_or_else(|| "bad input".to_string())?;
+    
+    let mut count = 0;
+    let mut recorded: Vec<Vec<bool>> = vec![vec![false; map[0].len()]; map.len()];
+    for (rownum, row) in map.iter().enumerate() {
+        let mut tallest = -1i8;
+        for (colnum, height) in row.iter().enumerate() {
+            if *height > tallest {
+                tallest = *height;
+                if !recorded[rownum][colnum] {
+                    recorded[rownum][colnum] = true;
+                    count += 1;
+                }
+            }
+        }
+
+        tallest = -1;
+
+        for (colnum, height) in row.iter().enumerate().rev() {
+            if *height > tallest {
+                tallest = *height;
+                if !recorded[rownum][colnum] {
+                    recorded[rownum][colnum] = true;
+                    count += 1;
+                }
+            }
+        }
+    }
+
+    for colnum in 0..map[0].len() {
+        let mut tallest = -1i8;
+        for (rownum, row) in map.iter().enumerate() {
+            let height = row[colnum];
+            if height > tallest {
+                tallest = height;
+                if !recorded[rownum][colnum] {
+                    recorded[rownum][colnum] = true;
+                    count += 1;
+                }
+            }
+        }
+
+        tallest  = -1i8;
+        for (rownum, row) in map.iter().enumerate().rev() {
+            let height = row[colnum];
+            if height > tallest {
+                tallest = height;
+                if !recorded[rownum][colnum] {
+                    recorded[rownum][colnum] = true;
+                    count += 1;
+                }
+            }
+        }
+    }
+
+    Ok(count.to_string())
 }
 
 pub fn part2(input: &str) -> Result<String, Box<dyn Error>> {
@@ -12,12 +70,11 @@ pub fn part2(input: &str) -> Result<String, Box<dyn Error>> {
 mod test {
     use super::*;
 
-    const INPUT: &str = include_str!("tests/day7test.txt");
+    const INPUT: &str = include_str!("tests/day8test.txt");
 
     #[test]
     fn test_part1() {
-        todo!("unimplemented");
-        assert_eq!(part1(INPUT).unwrap(), "")
+        assert_eq!(part1(INPUT).unwrap(), "21")
     }
 
     fn test_part2() {
