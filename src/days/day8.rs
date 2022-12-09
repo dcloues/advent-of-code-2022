90a5d905b0d943e63295073015d066de3057eb7f
Dustin Shields-Cloues <dustin@shields.clou.es>
2022-12-08 22:54:17 -0500

    Day 8, Part 2
    This is so incredibly terrible and I love it.

--- src/days/day8.rs
+++ src/days/day8.rs
@@ -1,7 +1,8 @@
 use std::error::Error;
 
-fn parse_map(input: &str) -> Result<Vec<Vec<i8>>,Box<dyn Error>> {
-    input.lines()
+fn parse_map(input: &str) -> Result<Vec<Vec<i8>>, Box<dyn Error>> {
+    input
+        .lines()
         .map(|l| l.chars().map(|c| c.to_digit(10).map(|d| d as i8)).collect())
         .collect::<Option<Vec<Vec<i8>>>>()
         .ok_or_else(|| "bad input".into())
@@ -49,7 +50,7 @@ pub fn part1(input: &str) -> Result<String, Box<dyn Error>> {
             }
         }
 
-        tallest  = -1i8;
+        tallest = -1i8;
         for (rownum, row) in map.iter().enumerate().rev() {
             let height = row[colnum];
             if height > tallest {
@@ -57,6 +58,7 @@ pub fn part1(input: &str) -> Result<String, Box<dyn Error>> {
                 if !recorded[rownum][colnum] {
                     recorded[rownum][colnum] = true;
                     count += 1;
+                    break;
                 }
             }
         }
@@ -66,7 +68,37 @@ pub fn part1(input: &str) -> Result<String, Box<dyn Error>> {
 }
 
 pub fn part2(input: &str) -> Result<String, Box<dyn Error>> {
-    todo!("unimplemented")
+    let map = parse_map(input)?;
+    let cols = map[0].len();
+    let mut best: u32 = 0;
+    for rownum in 1..map.len() - 1 {
+        let row = &map[rownum];
+        for colnum in 1..row.len() - 1 {
+            let tree = row[colnum];
+            let left = (0..colnum)
+                .rev()
+                .find(|oc| row[*oc] >= tree)
+                .map(|oc| colnum - oc)
+                .unwrap_or(colnum) as u32;
+            let right = (colnum + 1..row.len())
+                .find(|oc| row[*oc] >= tree)
+                .map(|oc| oc - colnum)
+                .unwrap_or(row.len() - colnum - 1) as u32;
+            let up = (0..rownum)
+                .rev()
+                .find(|or| map[*or][colnum] >= tree)
+                .map(|or| rownum - or)
+                .unwrap_or(rownum) as u32;
+            let down = (rownum + 1..map.len())
+                .find(|or| map[*or][colnum] >= tree)
+                .map(|or| or - rownum)
+                .unwrap_or(cols - rownum - 1) as u32;
+
+            best = best.max(left * right * up * down);
+        }
+    }
+
+    Ok(best.to_string())
 }
 
 #[cfg(test)]
@@ -80,8 +112,8 @@ mod test {
         assert_eq!(part1(INPUT).unwrap(), "21")
     }
 
+    #[test]
     fn test_part2() {
-        todo!("unimplemented");
-        assert_eq!(part2(INPUT).unwrap(), "")
+        assert_eq!(part2(INPUT).unwrap(), "8")
     }
 }
