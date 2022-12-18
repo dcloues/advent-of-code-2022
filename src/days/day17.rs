984c16e49b2f6b2b1ad83b6c7626e1afedf0341a
Dustin Shields-Cloues <dustin@shields.clou.es>
2022-12-18 09:01:22 -0500

    Day 17, Part 1

--- src/days/day17.rs
+++ src/days/day17.rs
@@ -1,11 +1,12 @@
 use std::{
     error::Error,
     fmt::{Display, Write},
+    time::Instant,
 };
 
 type Result<T> = std::result::Result<T, Box<dyn Error>>;
 
-#[derive(Debug, Clone, PartialEq, Eq)]
+#[derive(Debug, Copy, Clone, PartialEq, Eq)]
 enum Move {
     Left,
     Right,
@@ -47,8 +48,8 @@ fn pieces() -> Vec<Piece> {
         width: 3,
         height: 3,
         blocks: rev(vec![
-            false, false, true, false, false, false, false, false, false, true, false, false,
-            false, false, true, true, true, false, false, false, false,
+            false, false, false, false, true, false, false, false, false, false, false, true,
+            false, false, false, false, true, true, true, false, false,
         ]),
     };
 
@@ -57,8 +58,8 @@ fn pieces() -> Vec<Piece> {
         width: 1,
         height: 4,
         blocks: rev(vec![
-            true, false, false, false, false, false, false, true, false, false, false, false,
             false, false, true, false, false, false, false, false, false, true, false, false,
+            false, false, false, false, true, false, false, false, false, false, false, true,
             false, false, false, false,
         ]),
     };
@@ -68,7 +69,7 @@ fn pieces() -> Vec<Piece> {
         width: 2,
         height: 2,
         blocks: rev(vec![
-            true, true, false, false, false, false, false, true, true, false, false, false, false,
+            false, false, true, true, false, false, false, false, false, true, true, false, false,
             false,
         ]),
     };
@@ -78,7 +79,8 @@ fn pieces() -> Vec<Piece> {
 const BOARD_WIDTH: usize = 7;
 
 fn parse_input(s: &str) -> Result<Vec<Move>> {
-    s.chars()
+    s.trim()
+        .chars()
         .map(|c| match c {
             '<' => Ok(Move::Left),
             '>' => Ok(Move::Right),
@@ -88,19 +90,19 @@ fn parse_input(s: &str) -> Result<Vec<Move>> {
 }
 
 impl Piece {
-    fn apply_move(&mut self, mv: Move) {
+    fn apply_move(&mut self, mv: Move) -> bool {
         match mv {
             Move::Left if self.col > 0 => {
-                println!("Moving: {mv:?}");
                 self.col = self.col - 1;
                 self.blocks.rotate_right(1);
+                true
             }
             Move::Right if self.col + self.width < BOARD_WIDTH => {
-                println!("Moving: {mv:?}");
                 self.col = self.col + 1;
                 self.blocks.rotate_left(1);
+                true
             }
-            _ => {}
+            _ => false,
         }
     }
 }
@@ -109,64 +111,67 @@ struct Game<'a> {
     pieces: Box<dyn Iterator<Item = Piece> + 'a>,
     moves: Box<dyn Iterator<Item = Move> + 'a>,
     board: Vec<bool>,
-    piece_count: i32,
+    piece_count: i64,
 }
 
 impl<'a> Game<'a> {
     fn new(piece_spec: &'a Vec<Piece>, move_spec: &'a Vec<Move>) -> Self {
         Self {
-            pieces: Box::new(piece_spec.iter().cloned()),
-            moves: Box::new(move_spec.iter().cloned()),
+            pieces: Box::new(piece_spec.iter().cloned().cycle()),
+            moves: Box::new(move_spec.iter().cloned().cycle()),
             board: vec![],
             piece_count: 0,
         }
     }
     fn run_piece(&mut self) {
         self.piece_count += 1;
-        println!("---- spawning new piece ----");
         let mut piece = self.pieces.next().unwrap().clone();
         self.expand_board(piece.height);
         let mut piece_row = self.block_height() + 3;
-        println!("  set piece_row={piece_row}");
 
-        self.print_board(&piece, piece_row);
-
-        println!("---- beginning drop loop ----");
+        self.print_board(&piece, piece_row, "Spawned new piece");
         // Piece always starts out three rows above the top of the grid.
         // For these first three moves, we don't need collision checks,
         // because pieces cannot get stuck at this stage.
         for _ in 0..3 {
             // alternate applying gas jets...
-            piece.apply_move(self.moves.next().unwrap());
-            self.print_board(&piece, piece_row);
+            let mv = self.moves.next().unwrap();
+            let moved = piece.apply_move(mv);
+            if moved {
+                self.print_board(&piece, piece_row, &format!("Applied move {mv:?}"));
+            } else {
+                self.print_board(&piece, piece_row, &format!("Skipped move {mv:?} (wall)"));
+            }
             // and applying gravity
             piece_row -= 1;
-            self.print_board(&piece, piece_row);
+            self.print_board(&piece, piece_row, "Applied gravity");
         }
-
-        println!("---- preparing to land ----");
 
         // Now, we're overlapping with the board, which means pieces
         // can collide with the existing pieces.
         loop {
-            self.print_board(&piece, piece_row);
             let mv = self.moves.next().unwrap();
             let mut newpiece = piece.clone();
-            newpiece.apply_move(mv);
+            newpiece.apply_move(mv.clone());
             if !self.check_collision(&newpiece, piece_row) {
                 piece = newpiece;
             }
-
-            self.print_board(&piece, piece_row);
+            self.print_board(
+                &piece,
+                piece_row,
+                &format!("Applied move {mv:?} w/ collision detection"),
+            );
 
             if piece_row == 0 || self.check_collision(&piece, piece_row - 1) {
+                self.print_board(&piece, piece_row, "Piece landed");
                 self.land(piece, piece_row);
                 break;
             } else {
                 piece_row -= 1;
+                self.print_board(&piece, piece_row, "Applied gravity");
             }
 
-            self.print_board(&piece, piece_row);
+            //
         }
     }
 
@@ -179,8 +184,6 @@ impl<'a> Game<'a> {
                 self.board[start + i] = b;
             }
         }
-        // self.board
-        //     .splice(start..start + piece.blocks.len(), piece.blocks);
     }
 
     #[must_use]
@@ -195,7 +198,6 @@ impl<'a> Game<'a> {
 
     fn expand_board(&mut self, piece: usize) {
         let required = (self.block_height() + 3 + piece) * BOARD_WIDTH;
-        println!("resizing board to {required} rows");
         if self.board.len() < required {
             self.board.resize(required, false);
         }
@@ -207,38 +209,45 @@ impl<'a> Game<'a> {
 
     fn block_height(&self) -> usize {
         if let Some(last_set) = self.board.iter().rev().position(|b| *b) {
-            1 + (self.board.len() - last_set) / BOARD_WIDTH
+            (self.board.len() / BOARD_WIDTH) - (last_set / BOARD_WIDTH)
         } else {
             0
         }
-        // self.board.len()
-        //     - (self
-        //         .board
-        //         .iter()
-        //         .rev()
-        //         .position(|p| *p)
-        //         .unwrap_or(self.board_height())
-        //         / BOARD_WIDTH)
     }
 
-    fn print_board(&mut self, piece: &Piece, row: usize) {
+    fn print_board(&mut self, piece: &Piece, row: usize, desc: &str) {
+        return;
         let restore_board = self.board.clone();
         self.land(piece.clone(), row);
+        print!("{}[2J", 27 as char);
         println!("{self}");
+        println!(
+            "pieces={} board_height={} block_height={}",
+            self.piece_count,
+            self.board_height(),
+            self.block_height()
+        );
+        println!("> {desc}");
+        if self.piece_count > 28 {
+            let mut buffer = String::new();
+            std::io::stdin().read_line(&mut buffer);
+        }
         self.board = restore_board;
     }
 }
 
 impl<'a> Display for Game<'a> {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        for chunk in self.board.chunks(BOARD_WIDTH).rev() {
+        for (rowinv, chunk) in self.board.chunks(BOARD_WIDTH).enumerate().rev().take(40) {
             f.write_char('|')?;
             for b in chunk.iter().rev() {
                 f.write_char(if *b { '#' } else { '.' })?;
             }
             f.write_char('|')?;
+            f.write_str(&format!(" {}", rowinv))?;
             f.write_char('\n')?;
         }
+        f.write_str("+-------+\n")?;
         Ok(())
     }
 }
@@ -258,7 +267,28 @@ pub fn part1(input: &str) -> Result<String> {
 }
 
 pub fn part2(input: &str) -> Result<String> {
-    todo!("unimplemented")
+    let moves = parse_input(input)?;
+
+    let all_pieces = pieces();
+    let mut game = Game::new(&all_pieces, &moves);
+
+    let start = Instant::now();
+    let test_tick = 100000;
+    loop {
+        game.run_piece();
+        if game.piece_count == test_tick {
+            let now = Instant::now();
+            let elapsed = now - start;
+            println!("{test_tick} ticks in {}ms", elapsed.as_millis());
+            println!(
+                "estimated total runtime: {}s",
+                (elapsed * ((1000000000000 / test_tick) as u32)).as_secs()
+            );
+        }
+        if game.piece_count == 1000000000000 {
+            return Ok(game.block_height().to_string());
+        }
+    }
 }
 
 #[cfg(test)]
@@ -266,10 +296,41 @@ mod test {
     use super::*;
 
     const INPUT: &str = include_str!("tests/day17test.txt");
+    const LINE_BY_LINE: &str = include_str!("tests/day17testlines.txt");
 
     #[test]
     fn test_part1() {
         assert_eq!(part1(INPUT).unwrap(), "3068")
+    }
+
+    #[test]
+    fn test_part1_line_by_line() {
+        let moves = parse_input(INPUT).unwrap();
+        let expect: Vec<usize> = LINE_BY_LINE
+            .lines()
+            .map(|l| l.parse())
+            .collect::<std::result::Result<_, _>>()
+            .unwrap();
+
+        let all_pieces = pieces();
+        let mut game = Game::new(&all_pieces, &moves);
+
+        for height in expect {
+            game.run_piece();
+            println!(
+                "Piece={} expect={} got={}",
+                game.piece_count,
+                height,
+                game.block_height()
+            );
+            println!("{game}");
+            assert_eq!(
+                height,
+                game.block_height(),
+                "at iteration {}",
+                game.piece_count
+            );
+        }
     }
 
     fn test_part2() {
