2db3fd7c75f617d8881248cef207edb710b1b939
Dustin Shields-Cloues <dustin@shields.clou.es>
2022-12-24 07:29:41 -0500

    Day 20, Part 1

--- src/days/day20.rs
+++ src/days/day20.rs
@@ -1,4 +1,4 @@
-use std::{cell::RefCell, error::Error, rc::Rc};
+use std::{cell::Cell, error::Error};
 
 // Initial arrangement:
 // 0-7
@@ -20,12 +20,14 @@ use std::{cell::RefCell, error::Error, rc::Rc};
 // 0    1    4     2     5    4    6
 // [1], [2], [-2], [-3], [0], [3], [4]
 
+type ID = usize;
+
 #[derive(Eq)]
 struct Segment {
     id: usize,
     datum: i32,
-    previous: Option<Rc<RefCell<Segment>>>,
-    next: Option<Rc<RefCell<Segment>>>,
+    previous: Cell<ID>,
+    next: Cell<ID>,
 }
 
 impl PartialEq for Segment {
@@ -35,107 +37,96 @@ impl PartialEq for Segment {
 }
 
 struct Mixer {
-    input: Box<Vec<i32>>,
+    input: Vec<i32>,
     current: usize,
-    list: Vec<Rc<RefCell<Segment>>>,
-    head: Rc<RefCell<Segment>>,
+    list: Vec<Segment>,
+    head: ID,
 }
 
 impl Mixer {
     fn new(input: Vec<i32>) -> Self {
         let list = Self::build_segments(&input);
-        let head = list[0].clone();
-        let input = Box::new(input);
         Self {
             input,
             current: 0,
             list,
-            head,
+            head: 0,
         }
     }
 
-    fn build_segments(input: &Vec<i32>) -> Vec<Rc<RefCell<Segment>>> {
-        let segments: Vec<Rc<RefCell<Segment>>> = input
+    fn build_segments(input: &Vec<i32>) -> Vec<Segment> {
+        let segments: Vec<Segment> = input
             .iter()
             .enumerate()
-            .map(|(i, d)| {
-                Rc::new(RefCell::new(Segment {
-                    id: i,
-                    datum: *d,
-                    next: None,
-                    previous: None,
-                }))
+            .map(|(i, d)| Segment {
+                id: i,
+                datum: *d,
+                next: Cell::new(if i == input.len() - 1 { 0 } else { i + 1 }),
+                previous: Cell::new(if i == 0 { input.len() - 1 } else { i - 1 }),
             })
             .collect();
 
-        let head = segments[0].clone();
-        let tail = segments[1].clone();
-        head.borrow_mut().set_previous(tail.clone());
-        tail.borrow_mut().set_next(head.clone());
-
-        for window in segments.windows(2) {
-            if let [a, b] = window {
-                a.borrow_mut().next = Some(b.clone());
-                b.borrow_mut().previous = Some(a.clone());
-            } else {
-                panic!("unexpected match failure");
-            }
-        }
         segments
     }
 
-    fn mix_one(&mut self) -> Option<()> {
-        self.list.get(self.current).and_then(|seg| {
-            // TODO special handling if seg is the head!
-            let distance = self.input[self.current];
-            if distance > 0 {
-                if *seg == self.head {
-                    self.head = seg.borrow().next();
-                }
-
-                let new_previous = seg.borrow().nth(distance).clone();
-                seg.borrow_mut().remove();
-                seg.borrow_mut()
-                    .set_next(new_previous.borrow().next().clone());
-                new_previous
-                    .borrow()
-                    .next()
-                    .borrow_mut()
-                    .set_previous(seg.clone());
-                new_previous.borrow_mut().set_next(seg.clone());
-                seg.borrow_mut().set_previous(new_previous);
-            }
-
-            self.current += 1;
-            Some(())
-        })
+    fn mix_all(mut self) -> Vec<i32> {
+        while let Some(_) = self.mix_one() {}
+        self.result()
     }
 
-    // fn result<'a>(&'a self) -> Box<dyn Iterator<Item = i32> + 'a> {
-    //     let mut current = self.head.clone();
-    //     let mut done = false;
-    //     Box::new(std::iter::from_fn(|| {
-    //         if done {
-    //             None
-    //         } else {
-    //             let cs = current.borrow();
-    //             let value = cs.datum;
-    //             current = cs.next();
-    //             done = current == self.head;
-    //             Some(value)
-    //         }
-    //     }))
-    // }
+    fn mix_one(&mut self) -> Option<()> {
+        if self.current >= self.input.len() {
+            return None;
+        }
+
+        let current = &self.list[self.current];
+        if current.datum.is_positive() {
+            for _ in 0..current.datum {
+                let swapped = current.move_right(self);
+                if self.head == current.id {
+                    self.head = swapped.id;
+                }
+            }
+        } else if current.datum.is_negative() {
+            for _ in 0..current.datum.abs() {
+                let swapped = current.move_left(self);
+                if self.head == current.id {
+                    self.head = swapped.id;
+                }
+            }
+        }
+
+        self.current += 1;
+        debug_assert!(self.check_cycles().is_none());
+        Some(())
+    }
+
+    fn get(&self, id: ID) -> &Segment {
+        &self.list[id]
+    }
+
+    fn check_cycles(&self) -> Option<ID> {
+        let mut t = self.head;
+        let mut h = self.head;
+        loop {
+            t = self.get(t).next.get();
+            h = self.get(h).next.get();
+            h = self.get(h).next.get();
+            if t == self.head {
+                return None;
+            } else if t == h {
+                return Some(t);
+            }
+        }
+    }
 
     fn result(&self) -> Vec<i32> {
-        let mut current = self.head.clone();
+        let mut current = self.head;
         let mut data = Vec::with_capacity(self.input.len());
         loop {
-            data.push(current.borrow().datum);
-            current = {
-                let next = current.borrow().next();
-                next
-            };
+            let segment = self.get(current);
+            data.push(segment.datum);
+            current = segment.next.get();
             if current == self.head {
                 break;
             }
@@ -146,41 +137,50 @@ impl Mixer {
 }
 
 impl Segment {
-    fn nth(&self, n: i32) -> Rc<RefCell<Segment>> {
-        debug_assert!(n != 0);
-        match n {
-            1 => self.next(),
-            -1 => self.previous(),
-            _ if n > 1 => self.next().borrow().nth(n - 1),
-            _ if n < 1 => self.previous().borrow().nth(n + 1),
-            _ => panic!("invalid nth {n}"),
-        }
+    fn move_left<'a>(&self, mixer: &'a Mixer) -> &'a Segment {
+        let p1 = mixer.get(self.previous.get());
+        let p2 = mixer.get(p1.previous.get());
+        let n = mixer.get(self.next.get());
+        // From this:
+        // p2 <-> p1 <-> self <-> n
+        // To this:
+        // p2 <-> self <-> p1 <-> n
+        p2.set_next(self);
+        self.set_next(p1);
+        p1.set_next(n);
+        p1
     }
 
-    fn set_next(&mut self, next: Rc<RefCell<Segment>>) {
-        self.next = Some(next);
+    fn move_right<'a>(&self, mixer: &'a Mixer) -> &'a Segment {
+        // From this:
+        // p -> self -> n1 -> n2
+        // To this:
+        // p -> n1 -> self -> n2
+
+        let p = mixer.get(self.previous.get());
+        let n1 = mixer.get(self.next.get());
+        let n2 = mixer.get(n1.next.get());
+        p.set_next(n1);
+        n1.set_next(self);
+        self.set_next(n2);
+        n1
     }
 
-    fn set_previous(&mut self, previous: Rc<RefCell<Segment>>) {
-        self.previous = Some(previous);
-    }
-
-    fn remove(&self) {
-        self.previous().borrow_mut().set_next(self.next());
-        self.next().borrow_mut().set_previous(self.previous());
-    }
-
-    fn next(&self) -> Rc<RefCell<Segment>> {
-        self.next.as_ref().unwrap().clone()
-    }
-
-    fn previous(&self) -> Rc<RefCell<Segment>> {
-        self.previous.as_ref().unwrap().clone()
+    fn set_next(&self, next: &Segment) {
+        self.next.set(next.id);
+        next.previous.set(self.id);
     }
 }
 
-pub fn part1(_input: &str) -> Result<String, Box<dyn Error>> {
-    todo!("unimplemented")
+pub fn part1(input: &str) -> Result<String, Box<dyn Error>> {
+    let input: Vec<i32> = input.lines().map(|s| s.parse()).collect::<Result<_, _>>()?;
+    let result = Mixer::new(input).mix_all();
+    let i = result.iter().position(|i| *i == 0).unwrap();
+    let k1 = result[(i + 1000) % result.len()];
+    let k2 = result[(i + 2000) % result.len()];
+    let k3 = result[(i + 3000) % result.len()];
+    println!("i={i} k1={k1}, k2={k2}, k3={k3}");
+    Ok((k1 + k2 + k3).to_string())
 }
 
 pub fn part2(_input: &str) -> Result<String, Box<dyn Error>> {
@@ -191,7 +191,7 @@ pub fn part2(_input: &str) -> Result<String, Box<dyn Error>> {
 mod test {
     use super::*;
 
-    const INPUT: &str = include_str!("tests/day7test.txt");
+    const INPUT: &str = include_str!("tests/day20test.txt");
 
     #[test]
     fn test_mix() {
@@ -199,10 +199,20 @@ mod test {
         let mut mixer = Mixer::new(input);
         mixer.mix_one();
         assert_eq!(mixer.result(), vec![2, 1, -3, 3, -2, 0, 4]);
+        mixer.mix_one();
+        assert_eq!(mixer.result(), vec![1, -3, 2, 3, -2, 0, 4]);
+        mixer.mix_one();
+        assert_eq!(mixer.result(), vec![1, 2, 3, -2, -3, 0, 4]);
     }
 
     #[test]
-    #[ignore]
+    fn test_mix_all() {
+        let input = vec![1, 2, -3, 3, -2, 0, 4];
+        let result = Mixer::new(input).mix_all();
+        assert_eq!(result, vec![1, 2, -3, 4, 0, 3, -2]);
+    }
+
+    #[test]
     fn test_part1() {
         assert_eq!(part1(INPUT).unwrap(), "3")
     }
